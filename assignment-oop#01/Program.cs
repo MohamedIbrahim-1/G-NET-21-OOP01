2c8c6a525891ac022e6a5ccf4c1472794d86c555
mohamed ibrahim <mohamed ibrahim@MOHAMEDIBRAHIM>
2026-03-25 14:19:40 +0200

    Add project files.

---
+++ assignment-oop#01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace assignment_oop_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
